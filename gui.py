318e61c0912124f1af07cc120d16ce17849c15c6
Qweyke <kettenkrad1337@yandex.ru>
2025-03-08 23:13:12 +0500

    first attempts for 3d

--- gui.py
+++ gui.py
@@ -15,32 +15,48 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QMainWindow,
-    QPushButton, QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
+    QMainWindow, QPushButton, QSizePolicy, QStatusBar,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(644, 453)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout = QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.func_box = QComboBox(self.centralwidget)
-        self.func_box.setObjectName(u"func_box")
-
-        self.gridLayout.addWidget(self.func_box, 1, 2, 1, 1)
-
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.plot_btn = QPushButton(self.centralwidget)
         self.plot_btn.setObjectName(u"plot_btn")
 
-        self.gridLayout.addWidget(self.plot_btn, 0, 2, 1, 1)
+        self.verticalLayout.addWidget(self.plot_btn)
 
-        self.plot_wdgt = QWidget(self.centralwidget)
+        self.func_box = QComboBox(self.centralwidget)
+        self.func_box.setObjectName(u"func_box")
+
+        self.verticalLayout.addWidget(self.func_box)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.plot_wdgt = QFrame(self.centralwidget)
         self.plot_wdgt.setObjectName(u"plot_wdgt")
+        sizePolicy.setHeightForWidth(self.plot_wdgt.sizePolicy().hasHeightForWidth())
+        self.plot_wdgt.setSizePolicy(sizePolicy)
+        self.plot_wdgt.setFrameShape(QFrame.Shape.WinPanel)
 
-        self.gridLayout.addWidget(self.plot_wdgt, 0, 1, 2, 1)
+        self.horizontalLayout.addWidget(self.plot_wdgt)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
@@ -55,11 +71,11 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
 #if QT_CONFIG(whatsthis)
-        self.func_box.setWhatsThis(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Choose a function to plot</p></body></html>", None))
-#endif // QT_CONFIG(whatsthis)
-#if QT_CONFIG(whatsthis)
         self.plot_btn.setWhatsThis(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Press to plot chosen function</p></body></html>", None))
 #endif // QT_CONFIG(whatsthis)
         self.plot_btn.setText(QCoreApplication.translate("MainWindow", u"Plot", None))
+#if QT_CONFIG(whatsthis)
+        self.func_box.setWhatsThis(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Choose a function to plot</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
     # retranslateUi
 
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+"""pyside6-uic gui.py -o canvas.ui"""
+
 import sys
 
+import matplotlib.pyplot as plt
+import numpy as np
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+
+from funcs import calculate_func, Function
 from gui import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,13 +18,37 @@ class MainWindow(QMainWindow):
         self.gui = Ui_MainWindow()
         self.gui.setupUi(self)
 
-        # Window creation and resize as half of screen space
-        self.setWindowTitle("Qweyke Charter")
+        # Window resize as half of screen space
+        self.setWindowTitle("Qweyke Plotter")
         self.setMinimumSize(800, 600)
-        self.resize(int(QApplication.primaryScreen().geometry().width() * 0.5),
-                      int(QApplication.primaryScreen().geometry().height() * 0.5))
+        self.resize(int(QApplication.primaryScreen().geometry().width() * 0.7),
+                    int(QApplication.primaryScreen().geometry().height() * 0.7))
 
-        self.gui.plot_btn.connect()
+        self.figure = plt.figure()
+        self.subplot1 = self.figure.add_subplot(121, projection="3d")
+        self.subplot2 = self.figure.add_subplot(122, projection="3d")
+
+        x_list = np.linspace(0, 10, 100)
+        y_list = np.linspace(0, 10, 100)
+        z_list = calculate_func(Function.sin_mult, 1, 1000, 100)
+
+        self.subplot1.scatter(x_list, y_list, z_list)
+        self.subplot2.scatter(x_list, y_list, z_list)
+
+        self.figure_canvas = FigureCanvasQTAgg(self.figure)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.addWidget(self.figure_canvas)
+
+        self.gui.plot_wdgt.setLayout(layout)
+
+    #     self.gui.plot_btn.clicked.connect(lambda: self. plot_func(Function.sin_mult, 10))
+    #
+    # def plot_func(self, func_type, start, end, step):
+    #     print(calculate_func(func_type, start, end, step))
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
